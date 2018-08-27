d393f817dcb8daada91d4dd5159d06dec75dffa4
Jerridan Quiring <jerridan@gmail.com>
2018-08-27 00:51:06 -0400

    Creates function getPositionsByColumn to convert array of coordinates to positions object.

---
+++ src/getPositionsByColumn.js
@@ -0,0 +1,14 @@
+export default function getPositionsByColumn(coordinateList) {
+  return coordinateList.reduce((accumulator, coordinates) => {
+    if (accumulator[coordinates[0]]) {
+      return {
+        ...accumulator,
+        [coordinates[0]]: accumulator[coordinates[0]].concat(coordinates[1]),
+      };
+    }
+    return {
+      ...accumulator,
+      [coordinates[0]]: [coordinates[1]],
+    };
+  }, {});
+}
