837939f4e42043c6a4fc644aec5ee062faab83f9
Jerridan Quiring <jerridan@gmail.com>
2018-08-26 23:52:20 -0400

    Changes test descriptions / variable names.

--- src/getCoordinates.test.js
+++ src/getCoordinates.test.js
@@ -1,23 +1,23 @@
 import getCoordinates from "./getCoordinates";
 
 describe("getCoordinates", () => {
-  it("converts a hash of x-columns and their y coordinates to a double array off coordinates", () => {
-    const cells = {
+  it("converts a hash of positions by x-coordinates to an array of x,y coordinates", () => {
+    const cellPositions = {
       1: ["2"],
       2: ["3"],
     };
 
-    expect(getCoordinates(cells)).toEqual([["1", "2"], ["2", "3"]]);
+    expect(getCoordinates(cellPositions)).toEqual([["1", "2"], ["2", "3"]]);
   });
 
-  it("creates separate coordinates for a column with multiple y coordinates", () => {
-    const cells = {
+  it("creates separate coordinates for a column with multiple y-coordinates", () => {
+    const cellPositions = {
       1: ["5", "13"],
       3: ["0", "10", "4"],
       10: ["1"],
     };
 
-    expect(getCoordinates(cells)).toEqual([
+    expect(getCoordinates(cellPositions)).toEqual([
       ["1", "5"],
       ["1", "13"],
       ["3", "0"],
