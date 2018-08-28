58dee848fe6406d33a66defad2631cabd69db49c
Jerridan Quiring <jerridan@gmail.com>
2018-08-27 23:30:31 -0400

    Removes unnecessary tests.

--- src/calculateNextState.test.js
+++ src/calculateNextState.test.js
@@ -1,22 +1,6 @@
 import calculateNextState from "./calculateNextState";
 
 describe("calculateNextState", () => {
-  it("kills a cell with no neighbours", () => {
-    const cells = {
-      0: ["1"],
-    };
-
-    expect(calculateNextState(cells)).toEqual({});
-  });
-
-  it("kills a cell with only 1 neighbour", () => {
-    const cells = {
-      0: ["1", "2"],
-    };
-
-    expect(calculateNextState(cells)).toEqual({});
-  });
-
   it("kills a cell with fewer than 2 neighbours", () => {
     const cells = {
       0: ["10"],
