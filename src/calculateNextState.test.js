860db1204984b2d43854e3319c7b73d37412b720
Jerridan Quiring <jerridan@gmail.com>
2018-08-28 00:14:04 -0400

    Makes tests for calculateNextState more succinct.

--- src/calculateNextState.test.js
+++ src/calculateNextState.test.js
@@ -5,30 +5,20 @@ describe("calculateNextState", () => {
     const cells = {
       0: ["10"],
       1: ["1", "2"],
-      3: ["1", "2"],
-      4: ["1"],
     };
 
-    expect(calculateNextState(cells)).toEqual({
-      3: ["1", "2"],
-      4: ["1"],
-    });
+    expect(calculateNextState(cells)).toEqual({});
   });
 
   it("kills a cell with more than 3 neighbours", () => {
     const cells = {
-      3: ["1", "2"],
-      4: ["1"],
       6: ["1", "2", "3"],
-      7: ["1", "2", "3"],
-      8: ["1", "2", "3"],
+      7: ["1", "2"],
     };
 
     expect(calculateNextState(cells)).toEqual({
-      3: ["1", "2"],
-      4: ["1"],
       6: ["1", "3"],
-      8: ["1", "3"],
+      7: ["1"],
     });
   });
 });
