e7b784b7d49e8aeb6e61bc02823cf5d8040ad1b4
Jerridan Quiring <jerridan@gmail.com>
2019-11-12 23:49:43 -0500

    Updates getCellsFromPatternList to be able to list out the cells occupied given a single pattern at specified coordinates.

--- src/getCellsFromPatternList.js
+++ src/getCellsFromPatternList.js
@@ -1,3 +1,21 @@
-export default function getCellsFromPatternList(patterns) {
-  return patterns[0].pattern;
+import mapKeys from "lodash/mapKeys";
+import mapValues from "lodash/mapValues";
+
+export default function getCellsFromPatternList(lifeForms) {
+  const lifeForm = lifeForms[0];
+  const incrementedByX = incrementXCoordinates(lifeForm);
+  return incrementYCoordinates({
+    pattern: incrementedByX,
+    coordinates: lifeForm.coordinates,
+  });
 }
+
+const incrementXCoordinates = ({ pattern, coordinates }) =>
+  mapKeys(pattern, (y, x) => parseInt(x) + parseInt(coordinates[0]));
+
+const incrementYCoordinates = ({ pattern, coordinates }) =>
+  mapValues(pattern, yCoordinates => {
+    return yCoordinates.map(y =>
+      (parseInt(y) + parseInt(coordinates[1])).toString(),
+    );
+  });
--- src/getCellsFromPatternList.test.js
+++ src/getCellsFromPatternList.test.js
@@ -2,9 +2,14 @@ import getCellsFromPatternList from "./getCellsFromPatternList";
 import { beacon } from "./patterns";
 
 describe("getCellsFromPatternList", () => {
-  it("returns the coordinates of all cells given a single pattern, beginning at 0,0", () => {
-    const patterns = [{ pattern: beacon, coordinates: ["0", "0"] }];
+  it("returns the coordinates of all cells given a single pattern and non-zero coordinates", () => {
+    const lifeForms = [{ pattern: beacon, coordinates: ["10", "15"] }];
 
-    expect(getCellsFromPatternList(patterns)).toEqual(beacon);
+    expect(getCellsFromPatternList(lifeForms)).toEqual({
+      10: ["17", "18"],
+      11: ["18"],
+      12: ["15"],
+      13: ["15", "16"],
+    });
   });
 });
